f34701a6dbc9a5c473ba8af3bbc438f9d76f5589
Ash Rahman <asrahman97@gmail.com>
2020-01-27 18:36:30 -0500

    added ytplayer and autoplay on carousel

--- src/components/mobileGallery.js
+++ src/components/mobileGallery.js
@@ -3,7 +3,6 @@ import { Carousel } from 'react-responsive-carousel';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 
-import div1 from "../assests/home-background.jpg";
 import div2 from "../assests/home-background-5.jpg";
 import div3 from "../assests/home-background-7.jpg";
 import div4 from "../assests/home-background-1.jpg";
@@ -11,7 +10,6 @@ import div5 from "../assests/home-background-3.jpg";
 import div6 from "../assests/home-background-6.jpg";
 import div7 from "../assests/home-background-8.jpg";
 import div8 from "../assests/home-background-2.jpg";
-import div9 from "../assests/home-background-4.jpg";
 import div10 from "../assests/home-background-9.jpg";
 
 class MobileGallery extends Component {
