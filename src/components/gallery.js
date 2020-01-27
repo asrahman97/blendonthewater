8c133d49a8767e33c8bb8e6e71e601c044582ae0
Ash Rahman <asrahman97@gmail.com>
2020-01-27 10:52:19 -0500

    edited gallery

---
+++ src/components/gallery.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+class Gallery extends Component {
+  render() {
+    return (
+      <div className="gallery-section">
+        <h3>GALLERY</h3>
+
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Gallery;
