399a2a51f1ca8c18194429c1811e97b4a35c19ab
Ash Rahman <asrahman97@gmail.com>
2020-01-27 17:55:23 -0500

    added component

---
+++ src/components/mobileGallery.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+
+import div1 from "../assests/home-background.jpg";
+import div2 from "../assests/home-background-5.jpg";
+import div3 from "../assests/home-background-7.jpg";
+import div4 from "../assests/home-background-1.jpg";
+import div5 from "../assests/home-background-3.jpg";
+import div6 from "../assests/home-background-6.jpg";
+import div7 from "../assests/home-background-8.jpg";
+import div8 from "../assests/home-background-2.jpg";
+import div9 from "../assests/home-background-4.jpg";
+import div10 from "../assests/home-background-9.jpg";
+
+class MobileGallery extends Component {
+  render() {
+    return (
+      <div>
+        Sanity test
+      </div>
+    )
+  }
+}
+
+export default MobileGallery;
