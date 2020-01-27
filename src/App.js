ef5acf0621cfa6be1420610077f2889727358851
A Tawfique <easternselling@gmail.com>
2020-01-27 12:54:07 -0500

    [App.js] Bringing in menu component and rendering it

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header'
+import Menu from './components/Menu'
 
 class App extends React.Component {
 
@@ -16,6 +17,7 @@ class App extends React.Component {
     return (
       <div className="App" >
         <Header />
+        <Menu />
       </div>
     );
   }
