d592cf1fed22f1c6463ab6dd1f0a8ae2c76be1a5
A Tawfique <easternselling@gmail.com>
2020-01-27 10:53:47 -0500

    [Header.js ]Header component done

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,27 @@
 import React from 'react'
 import Logo from '../assests/blendlogo.png'
+import Twitter from '../assests/twittericon.png'
+import Facebook from '../assests/fbicon.png'
+import Instagram from '../assests/igicon.png'
 
 export default function Header() {
   return (
     <header>
-      <h2>Home</h2>
-      <h2>Reservation</h2>
-      <h2>Menu</h2>
+      <div className="header-section">
+        <h2>Home</h2>
+        <h2>Reservation</h2>
+        <h2>Menu</h2>
+      </div>
       <img src={Logo}></img>
-      <h2>Hours</h2>
-      <h2>About</h2>
-      <h2>Gallery</h2>
+      <div className="header-section">
+        <h2>Hours</h2>
+        <h2>About</h2>
+        <h2>Gallery</h2>
+      </div>
       <div className="social">
-        <h4>FB</h4>
-        <h4>TWT</h4>
-        <h4>IG</h4>
+        <img src={Facebook}></img>
+        <img src={Twitter}></img>
+        <img src={Instagram}></img>
       </div>
     </header>
   )
