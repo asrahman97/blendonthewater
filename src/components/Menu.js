a83c69ce77798d513e0cf0bd9eefa32191900586
A Tawfique <easternselling@gmail.com>
2020-01-27 12:53:46 -0500

    [Menu] Added menu component and rendered section header.

---
+++ src/components/Menu.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Menu() {
+  return (
+    <div className="menu" id="menu">
+      <div className="menu-section-header">
+        <div className="margin-bottom left"></div>
+        <h1>Menu</h1>
+        <div className="margin-bottom right"></div>
+      </div>
+    </div>
+  )
+}
