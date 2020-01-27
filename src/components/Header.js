4c890ce6b079e1ed77ead3cb1b313e4836df4d8b
sweinstein <raeweinst@gmail.com>
2020-01-27 18:45:11 -0500

    add burger menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { slide as Menu } from 'react-burger-menu'
+
 import Logo from '../assests/blendlogo.png'
 import Twitter from '../assests/twittericon.png'
 import Facebook from '../assests/fbicon.png'
@@ -6,23 +8,35 @@ import Instagram from '../assests/igicon.png'
 
 export default function Header() {
   return (
-    <header>
-      <div className="header-section">
+    <div>
+      <Menu>
         <h2><a href="#video">Home</a></h2>
-        <h2><a href="#reservations">Reservation</a></h2>
         <h2><a href="#menu">Menu</a></h2>
-      </div>
-      <img src={Logo}></img>
-      <div className="header-section">
         <h2><a href="#info">Hours</a></h2>
         <h2><a href="#about">About</a></h2>
         <h2><a href="#gallery">Gallery</a></h2>
-      </div>
-      <div className="social">
-        <img src={Facebook}></img>
-        <img src={Twitter}></img>
-        <img src={Instagram}></img>
-      </div>
-    </header>
+        <h2><a href="#reservations">Reservation</a></h2>
+
+      </Menu>
+      <header>
+
+        <div className="header-section">
+          <h2><a href="#video">Home</a></h2>
+          <h2><a href="#reservations">Reservation</a></h2>
+          <h2><a href="#menu">Menu</a></h2>
+        </div>
+        <img src={Logo}></img>
+        <div className="header-section">
+          <h2><a href="#info">Hours</a></h2>
+          <h2><a href="#about">About</a></h2>
+          <h2><a href="#gallery">Gallery</a></h2>
+        </div>
+        <div className="social">
+          <img src={Facebook}></img>
+          <img src={Twitter}></img>
+          <img src={Instagram}></img>
+        </div>
+      </header>
+    </div>
   )
 }
