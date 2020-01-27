a5b9aef2eca3252abfb6e59dd6130d1aea32eeee
Ash Rahman <asrahman97@gmail.com>
2020-01-27 12:30:25 -0500

    css added for gallery

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -3,10 +3,10 @@ import React, { Component } from "react";
 class Gallery extends Component {
   render() {
     return (
-      <div className="gallery-section">
-        <h3>GALLERY</h3>
+      <div className="gallery-section" id='gallery'>
+        <h3 className='gallery-title'>GALLERY</h3>
 
-        <p>
+        <p className='gallery-body'>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
           minim veniam, quis nostrud exercitation ullamco laboris nisi ut
