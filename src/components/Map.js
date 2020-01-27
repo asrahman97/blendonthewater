39974c1fcdd9986df24ac64b0b863c483b8f9e6b
sweinstein <raeweinst@gmail.com>
2020-01-27 11:44:49 -0500

    add location section

--- src/components/Map.js
+++ src/components/Map.js
@@ -3,11 +3,14 @@ const place = 'place_id:ChIJaQR-pCFZwokRNMloynEQPgA'
 const key = 'AIzaSyA5j2U5sxqhJ4FYNWev0rSR4dyF5N5VFDk'
 function Map(){
   return (
-    <iframe
-      title="google map"
-      className = "map"
-      src={`https://www.google.com/maps/embed/v1/place?q=${place}&key=${key}`}>
-    </iframe>
+    <div>
+      <iframe
+        title="google map"
+        className = "map"
+        src={`https://www.google.com/maps/embed/v1/place?q=${place}&key=${key}`}>
+      </iframe>
+    </div>
+
   )
 }
 export default Map;
