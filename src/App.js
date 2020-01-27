e7ff6833c123a07c3171239dd35ce22fe661a102
A Tawfique <easternselling@gmail.com>
2020-01-27 16:34:58 -0500

    Changed text for video

--- src/App.js
+++ src/App.js
@@ -22,16 +22,16 @@ class App extends React.Component {
     return (
       <div className="App" >
         <Header />
-        <div id = "video">
+        <div id="video">
           <iframe
             id="ytplayer"
             src="https://www.youtube.com/embed/videoseries?controls=0&amp;list=PL9CSmGF_Y-0bnYfzXYKZ42f-HY2lRAWLd&autoplay=1&loop=1&controls=0&mute=1"
             frameborder="0">
           </iframe>
-          <div id = "video-text">
-            <h1>Innovative Solutions to boost your creative projects</h1>
+          <div id="video-text">
+            <h1>A place to eat, meet and greet.</h1>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
-            <a href = '#reservations'><button>RESERVATIONS</button></a>
+            <a href='#reservations'><button>RESERVATIONS</button></a>
           </div>
         </div>
         <Menu />
@@ -39,12 +39,12 @@ class App extends React.Component {
         <About />
         <Gallery />
         <GalleryGrid />
-        <div id = "location" >
+        <div id="location" >
           <h2>LOCATION</h2>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
           <p>9569393392 <br />
             info@realwire.com <br />
-          4540 Center Blvd. Long Island City, NY, 11101 <br />
+            4540 Center Blvd. Long Island City, NY, 11101 <br />
           </p>
         </div>
         <Map />
