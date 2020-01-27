f58da67b86ea2d7805b0a2d3e2d07f1c8a108447
sweinstein <raeweinst@gmail.com>
2020-01-27 13:50:05 -0500

    add embedded video

--- src/App.js
+++ src/App.js
@@ -17,6 +17,13 @@ class App extends React.Component {
     return (
       <div className="App" >
         <Header />
+        <div id = "video">
+          <iframe
+            id="ytplayer"
+            src="https://www.youtube.com/embed/videoseries?controls=0&amp;list=PL9CSmGF_Y-0bnYfzXYKZ42f-HY2lRAWLd&autoplay=1&loop=1&controls=0&mute=1"
+            frameborder="0"
+          ></iframe>
+        </div>
         <div id = "location" >
           <h2>LOCATION</h2>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
